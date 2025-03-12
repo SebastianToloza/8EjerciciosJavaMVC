a7be73487284864889f3633e6afa57e2e7343d96
SebastianToloza <sebastiantoloza168@gmail.com>
2025-03-12 09:37:57 -0500

    Add files via upload

---
+++ SistemaReservasVector/src/sistemareservasvector/SistemaReservasVector.java
@@ -0,0 +1,12 @@
+
+package sistemareservasvector;
+
+public class SistemaReservasVector {
+    public static void main(String[] args) {
+        Cliente cliente2 = new Cliente();
+        Cliente usuario=cliente2.guardarCliente();
+        usuario.imprimirInformacion();
+        
+    }
+    
+}
